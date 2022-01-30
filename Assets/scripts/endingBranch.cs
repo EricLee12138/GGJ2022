14ca1f4cc6bd47e622c8c982798fd1d92787d567
Rosy <spatulag@gmail.com>
2022-01-30 13:56:15 -0800

    ending branch modify, adding evil/son point check

--- Assets/scripts/endingBranch.cs
+++ Assets/scripts/endingBranch.cs
@@ -5,47 +5,67 @@ using UnityEngine;
 
 public class endingBranch : MonoBehaviour
 {
+    [Range(0,10)]
+    public int sonPointPass;//if greater than sonPointPass you are good mom
+    [Range(0,10)]
+    public int evilPointPass;//if greater than evilPointPass you are a$$hole
+    
     public GameFlagManager gameFlagManager;
     public string sonText;
     public string neighborText;
-    struct EndingBranch
-    {
+    struct EndingBranch {
         public string gameFlag;
-        public string branchBoby;
+        public string branchBody;
         public bool isFromSon;
+        public string evilText;
+        public string goodText;
 
-        public EndingBranch(string gameFlag, string branchBoby, bool isFromSon)
-        {
+        public EndingBranch(string gameFlag, string evilText, string goodText, bool isFromSon) {
             this.gameFlag = gameFlag;
-            this.branchBoby = branchBoby;
+            this.goodText = goodText;
+            this.evilText = evilText;
+            branchBody = "";
             this.isFromSon = isFromSon;
+        }
+
+        public void setBranchBody(string text)
+        {
+            this.branchBody = text;
         }
     };
 
-    private List<EndingBranch> endingBranches;
+    private List<EndingBranch> endingBranches = new List<EndingBranch>();
     private void Start() {
-        endingBranches.Add(new EndingBranch("isSteal","She stole food from our...",false));
-        endingBranches.Add(new EndingBranch("isColdKid","I remember the lady as...",false));
-        endingBranches.Add(new EndingBranch("isHelpHobo","I appreciate...",false));
-        endingBranches.Add(new EndingBranch("isHospital","We worked on the surgery whilst...",false));
-        endingBranches.Add(new EndingBranch("isSon","Mom's a hero...",false));
-        endingBranches.Add(new EndingBranch("isMom","I had to...",false));
-        endingBranches.Add(new EndingBranch("isEeyore","carrot",false));
-        endingBranches.Add(new EndingBranch("isJoinWar","She decided to join the war...",false));
+        endingBranches.Add(new EndingBranch("isSteal","","She stole food from our...",false));
+        endingBranches.Add(new EndingBranch("isColdKid","","I remember the lady as...",false));
+        endingBranches.Add(new EndingBranch("isHelpHobo","","I appreciate...",false));
+        endingBranches.Add(new EndingBranch("isHospital","","We worked on the surgery whilst...",false));
+        endingBranches.Add(new EndingBranch("isSon","","Mom's a hero...",false));
+        endingBranches.Add(new EndingBranch("isMom","","I had to...",false));
+        endingBranches.Add(new EndingBranch("isEeyore","","carrot",false));
+        endingBranches.Add(new EndingBranch("isJoinWar","","She decided to join the war...",false));
         gameFlagManager = this.GetComponent<GameFlagManager>();
+        foreach (EndingBranch endingBranch in endingBranches) {
+            if (endingBranch.isFromSon) {            
+                endingBranch.setBranchBody(gameFlagManager.sonPoint > sonPointPass ? endingBranch.goodText : endingBranch.evilText);
+            }
+            else {
+                endingBranch.setBranchBody(gameFlagManager.evilPoint > evilPointPass ? endingBranch.evilText : endingBranch.goodText);
+            }
+        }
     }
 
     public string getSonText() {
         string _sonText = "";
         foreach (var branch in endingBranches) {
             if (branch.isFromSon == true) {
-                if (gameFlagManager.hasFlag(branch.gameFlag))
-                {
-                    _sonText += branch.branchBoby;
+                if (gameFlagManager.hasFlag(branch.gameFlag)) {
+                    _sonText += branch.branchBody;
+                    _sonText += "\n";
                 }
             }
         }
-
+        
         sonText = _sonText;
         return _sonText;
     }
@@ -55,16 +75,14 @@ public class endingBranch : MonoBehaviour
         string _neighborText = "";
         foreach (var branch in endingBranches) {
             if (branch.isFromSon == false) {
-                if (gameFlagManager.hasFlag(branch.gameFlag))
-                {
-                    _neighborText += branch.branchBoby;
+                if (gameFlagManager.hasFlag(branch.gameFlag)) {
+                    _neighborText += branch.branchBody;
+                    _neighborText += "\n";
                 }
             }
         }
-
+        
         neighborText = _neighborText;
         return _neighborText;
-        
     }
-
 }
